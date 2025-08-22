02266d0c7412a1f65e496f3ca8054f4a8bca2672
Chiwaku <chiwaku@gmail.com>
2025-08-22 18:21:37 +0800

    Updating hamburger menu

--- js/all.js
+++ js/all.js
@@ -1,5 +1,7 @@
 const $navInfo = $('#NavInfo');
 const $navTrain = $('#NavTrain');
+const $navMenu = $('#NavMenu');
+const $navList = $('#NavList')
 
 //Init 
 
@@ -19,3 +21,18 @@ $navTrain
     .on('mouseleave', function () {
         $(this).find('ul').hide()
     })
+
+$navMenu
+    .on('click', function () {
+        $navList.toggleClass('active')
+    })
+
+$(window).on('resize', function () {
+    if ($(window).width() > 565) {
+        $navList.removeClass('active').css('display', 'flex');
+    } else {
+        $navList.css('display', '');
+    }
+});
+
+$(window).trigger('resize');
